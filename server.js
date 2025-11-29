783211ae13a9fad28f7c55f774ef34c2bad28c86
Production VM <production@flipperpro.local>
2025-11-29 20:20:33 +0000

    Update CORS: allow flipper-pro.com and www.flipper-pro.com

--- server.js
+++ server.js
@@ -25,6 +25,16 @@ app.use(cors({
  },
  methods: [\GET\],
  allowedHeaders: [\Content-Type\, \X-FLIPPER-SECRET\]
+}));
+        const allowedOrigins = [" https://flipper-pro.com\, \https://www.flipper-pro.com\];
+ if (allowedOrigins.indexOf(origin) !== -1) {
+ callback(null, true);
+ } else {
+ callback(new Error(\Not allowed by CORS\));
+ }
+ },
+ methods: [\GET\],
+ allowedHeaders: [\Content-Type\, \X-FLIPPER-SECRET\]
         }
         const allowedOrigins = ["https://flipper-pro.com", "https://www.flipper-pro.com"];
         if (allowedOrigins.indexOf(origin) !== -1) {
